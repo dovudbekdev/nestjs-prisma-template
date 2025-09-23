05ca7b51448cefbd533345de486f5faac4ddcf46
dovudbekdev <oktamovdovudbek030415@gmail.com>
2025-09-23 15:38:33 +0500

    Xatolik to'g'irlandi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
-
+import { PrismaModule } from './modules/prisma';
 
 @Module({
-  imports: [UserModule]
+  imports: [UserModule, PrismaModule],
 })
 export class AppModule {}
